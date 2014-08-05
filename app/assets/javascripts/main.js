bdc75cdc7247cb5cd72de562e05f4476d8219e36
Jodie Butterworth <pannajb@yahoo.com>
2014-08-05 11:35:27 -0400

    initial commit

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+	$('.error_number').keypress(function(e){
+	 	if (e.keyCode==13)
+	 		BadNumber($('.error_number').val());
+	});
+
+	function BadNumber(num){
+		try { if (badNum=isNaN(num))
+			throw "why?"
+		} catch(err){
+			alert ("Please enter a number.");
+		} finally{
+			console.log ('The BadNumber function is done now.');
+		}
+	};
+
+
+	$('.nothing').click(function(){
+		var x = {};
+			x.first();
+	});
+
+		try {
+			var x = {};
+			x.first();
+			} catch (err){
+				alert (err.message, err.lineNumber);
+			}
+			finally {
+				console.log('all done!');
+			}
+});
+
+window.onerror = function(msg, url, line){
+	alert ('Error: ' + msg + ' Script: ' + url + ' Line: ' + line);
+	return true;
+}
